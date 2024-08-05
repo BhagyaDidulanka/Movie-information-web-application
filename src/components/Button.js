547e0e5840b2248d2598f48cfe50d6c2fe749d62
BhagyaDidulanka <didulankabhagya@gmail.com>
2024-08-05 12:06:59 +0530

    create PageNotFound page

---
+++ src/components/Button.js
@@ -0,0 +1,5 @@
+export const Button = ({children}) => {
+  return (
+    <button className='rounded p-2.5 font-medium bg-gradient-to-r from-cyan-500 to-blue-500'>{children}</button>
+  )
+}
--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -1,5 +1,23 @@
+import { Link } from 'react-router-dom';
+import pageNotFound from '../assets/images/pagenotfound.png';
+import { Button } from '../components';
+
 export const PageNotFound = () => {
   return (
-    <main>PageNotFound</main>
+    <main>
+      <section className='flex flex-col items-center justify-center '>
+          <div>
+            <p className='flex flex-col items-center py-8 text-5xl dark:text-gray-200'>404 Oops!</p>
+          </div>
+          <div className='max-w-lg'>
+            <img className='rounded-md' src={pageNotFound} alt="Page Not Found" />
+          </div>
+          <div className='flex items-center justify-center py-8'>
+            <Link to="/">
+            <Button>Back To Home</Button>
+            </Link>
+          </div>
+      </section>
+    </main>
   )
 }
